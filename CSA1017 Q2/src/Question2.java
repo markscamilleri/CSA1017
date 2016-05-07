5b72f4591458469caed55de4ba09f0580eee8204
Mark Said Camilleri <markscamilleri@users.noreply.github.com>
2016-05-07 18:32:30 +0200

    Added Inline Documentation

--- CSA1017 Q2/src/Question2.java
+++ CSA1017 Q2/src/Question2.java
@@ -2,53 +2,71 @@ import java.util.Scanner;
 
 /**
  * Created by mark on 09/02/16.
+ * This answer assumes the RPN input is correct.
  */
 public class Question2 {
-
     public static void main(String args[]) {
 
         //Initialize Scanner object
         Scanner in = new Scanner(System.in).useDelimiter("\n");
 
+        /*======================== WELCOME MESSAGE TO USER =======================*/
         System.out.println("_____________________________________________________");
         System.out.println("|    CSA 1017 - Data Structures and Algorithms 1    |");
         System.out.println("|---------------------------------------------------|");
         System.out.println("|         Submission by Mark Said Camilleri         |");
         System.out.println("|     Task 2: Reverse Polish Notation evaluator     |");
         System.out.println("|---------------------------------------------------|");
-        System.out.print("| Please enter a number to convert: ");
+        System.out.print("| Please enter a number to convert: "); //prompt for user input. Assumes correctness.
 
+        //Initialized a stack object (using the stack defined here). Note, no importing of the Stack class.
         Stack<Double> nums = new Stack<>();
 
+        //Reads user input. Must be a valid RPN expression.
         String expression = in.next();
         System.out.println("|---------------------------------------------------|");
-        System.out.println("| Contents of the stack at each step:               |");
+        System.out.println("| Contents of the stack at each step:               |");//some message to user.
 
+        //Iterates through the string inputted by the user.
         for (int i = 0; i < expression.length(); i++) {
             char cChar = expression.charAt(i);
 
+            //If the current character is a space, nothing needs to be done.
             if (Character.isWhitespace(cChar)) continue;
+                //If it's a '+', then 2 numbers are popped, added and the answer is pushed onto the stack.
             else if (cChar == '+') {
                 double num1 = nums.pop();
                 double num2 = nums.pop();
 
                 nums.push(num2 + num1);
-            } else if (cChar == '-') {
+            }
+            //If it's a '-', then 2 numbers are popped, subtracted and the answer is pushed onto the stack.
+            else if (cChar == '-') {
                 double num1 = nums.pop();
                 double num2 = nums.pop();
 
                 nums.push(num2 - num1);
-            } else if (cChar == '*') {
+            }
+            //If it's a '*', then 2 numbers are popped, multiplied and the answer is pushed onto the stack.
+            else if (cChar == '*') {
                 double num1 = nums.pop();
                 double num2 = nums.pop();
 
                 nums.push(num2 * num1);
-            } else if (cChar == '/') {
+            }
+            //If it's a '/', then 2 numbers are popped, divided and the answer is pushed onto the stack.
+            else if (cChar == '/') {
                 double num1 = nums.pop();
                 double num2 = nums.pop();
 
                 nums.push(num2 / num1);
-            } else {
+            }
+            /*
+             * Otherwise, assuming it's inputted correctly, the character must be a number.
+             * In which case it is converted ot a double (allowing for any real number to be inputted)
+             * and pushed onto the stack.
+             */
+            else {
                 int start = i;
                 while (Character.isDigit(expression.charAt(i)) || expression.charAt(i) == '.') i++;
 
@@ -56,9 +74,10 @@ public class Question2 {
             }
             System.out.printf("| %-49s |\n", nums.toString());
         }
+
+        //When the above iteration is complete, there should only be one item on the stack whcih is the answer.
         System.out.println("|---------------------------------------------------|");
         System.out.printf("| Answer of Evaluation = %-26s |\n", nums.pop().toString());
-        System.out.println("|___________________________________________________2|");
-
+        System.out.println("|___________________________________________________|");
     }
 }
--- CSA1017 Q2/src/Stack.java
+++ CSA1017 Q2/src/Stack.java
@@ -3,34 +3,65 @@ import java.util.Collection;
 
 /**
  * Created by mark on 14/02/16.
+ *
+ * A stack implemented as an ArrayList to have it dynamically increase its size.
  */
 public class Stack<E> extends ArrayList {
 
+    /**
+     * Default constructor. Calls the ArrayList default constructor
+     */
     public Stack() {
+        super();
     }
 
+    /**
+     * Initialised a stack with the contents of the Collection in the parameter.
+     *
+     * @param c the contents to initialise the stack with.
+     */
     public Stack(Collection<? extends E> c) {
         super(c);
     }
 
-    public void push(E data) {
+    /**
+     * Pushes the data onto the stack
+     *
+     * @param data data to be pushed on the stack
+     * @return true if successfully pushed. false otherwise (stack is full?)
+     */
+    public boolean push(E data) {
         int prevSize = this.size();
         this.add(data);
 
+        //Condition to check if the data has been successfully added.
         if (this.size() == prevSize + 1) return true;
         else return false;
     }
 
+    /**
+     * Pops the topmost item from the stack.
+     * @return the data from the top f the stack is not empty. null if it is empty.
+     */
     public E pop() {
         if (this.size() == 0) return null;
         else return (E) this.remove(this.size() - 1);
     }
 
+    /**
+     * Returns the data at the top of the stack without popping it.
+     * @return the data if the stack is not empty. null if it is empty.
+     */
     public E peek() {
         if (this.size() == 0) return null;
         else return (E) this.get(this.size() - 1);
     }
 
+    /**
+     * Returns a string representation of the contents of the stack.
+     * @overrides toString() in class AbstractCollection<E>
+     * @return The string representation of the ArrayList if not empty. "Stack is empty" if it is empty.
+     */
     public String toString() {
         if (this.size() == 0) return "Stack is empty";
         else return super.toString();
