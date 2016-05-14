4b28af1f8c844be4b977a7061953df2513f056a0
Mark Said Camilleri <markscamilleri@users.noreply.github.com>
2016-05-14 17:02:36 +0200

    Added inline comments

--- CSA1017 Q4/src/Question4.java
+++ CSA1017 Q4/src/Question4.java
@@ -6,10 +6,11 @@ import java.util.Scanner;
  */
 public class Question4 {
 
-    public static final int SIZE = 16384;
+    public static final int SIZE = 10;
 
     public static void main(String[] args) {
 
+        /*======================== WELCOME MESSAGE TO USER =======================*/
         System.out.println("_____________________________________________________");
         System.out.println("|    CSA 1017 - Data Structures and Algorithms 1    |");
         System.out.println("|---------------------------------------------------|");
@@ -18,34 +19,74 @@ public class Question4 {
         System.out.println("|---------------------------------------------------|");
         System.out.println("| The array to be sorted is:                        |");
 
+        //The array, which  will eventially be sort
         int[] arr = new int[SIZE];
 
+        //Populating the array with random data.
         for (int i = 0; i < arr.length; i++) {
             arr[i] = (int) ((Math.random() + 1) * SIZE);
         }
 
-        int[] sorted = shellSort(arr);
-        System.out.print("Unsorted Array: ");
+        //Displays the unsorted array to the user.
         System.out.println(Arrays.toString(arr));
 
+        //Sorts the array
+        int[] sorted = shellSort(arr);
+
+        //Displays the sorted array to the user
         System.out.print("Sorted Array: ");
         System.out.println(Arrays.toString(sorted));
 
     }
 
+    /**
+     * Sorts the inputted array using the shell sort algorithm.
+     *
+     * @param unsorted The array to be sorted
+     * @return The sorted version of the array
+     */
     private static int[] shellSort(int[] unsorted) {
+
+        /*
+         * First, the program begins my making a deep copy of the
+         * array to make sure the original one isn't affected.
+         */
         int[] sorted = Arrays.copyOf(unsorted, SIZE);
 
-        for (int sep = ((int) Math.pow(2, Math.log(SIZE) / Math.log(2) - 1) - 1); sep > 0; sep = (sep + 1) / 2 - 1) {
+        /*
+         * This loop begins with the iterator being set at half the
+         * size of the array, - 1. This then iterates downwards by
+         * incrementing the iterator, dividing it by 2 and subtracting
+         * 1 from it, until it performs it's final iteration at a
+         * value of 1.
+         */
+        for (int sep = (unsorted.length / 2) - 1; sep > 0; sep = (sep + 1) / 2 - 1) {
+            /*
+             * This for loop goes through the items between sep and
+             * the end of the array. To then use an bu sort
+             * algorithm to switch it
+             */
             for (int i = sep; i < SIZE; i++) {
+                /*
+                 * The algorthm begins by storing the value in a
+                 * temporary variable.
+                 */
                 int temp = sorted[i];
 
+                //iterator for next for loop. Used also outside loop.
                 int j;
-                //Finds position of temp
+                /*
+                 * Finds good position of temp in steps of sep, sort
+                 * of like insertion sort.
+                 */
                 for (j = i; j >= sep && sorted[j - sep] > temp; j -= sep) {
-                    //swap
+                    /*
+                     * Moves the (j-sep)th by a step of sep to
+                     * leave space for temp at it's proper place.
+                     */
                     sorted[j] = sorted[j - sep];
                 }
+                //Once found temp is stored in it's proper place.
                 sorted[j] = temp;
             }
         }
