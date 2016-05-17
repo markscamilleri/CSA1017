8abbe0fe099321c9143827ab1908fc8cf71f649a
Mark Said Camilleri <markscamilleri@users.noreply.github.com>
2016-05-17 20:49:56 +0200

    Finished commenting

--- CSA1017 Q6/src/Question6.java
+++ CSA1017 Q6/src/Question6.java
@@ -1,12 +1,17 @@
 /**
- * Created by mark on 18/02/16.
+ * @author Mark Said Camilleri
+ * @version 20160517
  */
 public class Question6 {
 
-    public static final int SIZE = 32;
+    /**
+     * Stores the size of the matricie
+     */
+    public static final int SIZE = 3;
 
     public static void main(String[] args) {
 
+        /*=========================== WELCOME USER ================================*/
         System.out.println("_____________________________________________________");
         System.out.println("|    CSA 1017 - Data Structures and Algorithms 1    |");
         System.out.println("|---------------------------------------------------|");
@@ -14,21 +19,24 @@ public class Question6 {
         System.out.println("|           Task 6: Matrix Multiplication           |");
         System.out.println("|---------------------------------------------------|");
 
+        /*
+         * The 2 matricies. Matrix A will be multuplied with matrix B
+         * to from matrix AB.
+         */
         double[][] matrixA = new double[SIZE][SIZE];
         double[][] matrixB = new double[SIZE][SIZE];
-
 
         //Put random values from 1 to 10 in matrixA
         for (int i = 0; i < SIZE; i++) {
             for (int j = 0; j < SIZE; j++) {
-                matrixA[i][j] = Math.random() * 10 + 1;
+                matrixA[i][j] = Math.random() * 20 - 10;
             }
         }
 
         //Put random values from 1 to 10 in matrixB
         for (int i = 0; i < SIZE; i++) {
             for (int j = 0; j < SIZE; j++) {
-                matrixB[i][j] = Math.random() * 10 + 1;
+                matrixB[i][j] = Math.random() * 20 - 10;
             }
         }
 
@@ -36,49 +44,35 @@ public class Question6 {
         double[][] result = new double[SIZE][SIZE];
 
 
-        //Transpose MatrixB
+       /*
+        * Transposes MatrixB. This is done to make the multiplication
+        * easier since the method would accept 2 arrays, one for the
+        * row and the other for the column.
+        */
         double[][] matBtranspose = matTranspose(matrixB);
+
+       /*
+        * Multiplies each row of Matrix A with each column of Matrix B
+        * and adds them up to get the value of each cell.
+        */
 
         for (int i = 0; i < SIZE; i++) {
             for (int j = 0; j < SIZE; j++) {
                 result[i][j] = cellMult(matrixA[i], matBtranspose[j]);
             }
         }
-
+        //Outputs Matrix A
         System.out.print("Matrix A = ");
-        for (int i = 0; i < SIZE; i++) {
+        displayMatrix(matrixA);
 
-            for (int j = 0; j < SIZE; j++) {
-                System.out.printf("%5.2f ", matrixA[i][j]);
-            }
-            System.out.println();
-            System.out.print("           ");
 
-        }
         System.out.println();
         System.out.print("Matrix B = ");
-        for (int i = 0; i < SIZE; i++) {
-
-            for (int j = 0; j < SIZE; j++) {
-                System.out.printf("%5.2f ", matrixB[i][j]);
-            }
-            System.out.println();
-            System.out.print("           ");
-
-        }
+        displayMatrix(matrixB);
 
         System.out.println();
         System.out.print("  A * B =  ");
-        for (int i = 0; i < SIZE; i++) {
-
-            for (int j = 0; j < SIZE; j++) {
-                System.out.printf("%7.2f ", result[i][j]);
-            }
-            System.out.println();
-            System.out.print("           ");
-
-        }
-
+        displayMatrix(result);
 
     }
 
@@ -89,7 +83,8 @@ public class Question6 {
      * @param col The colum of the second matrix to be multiplied
      * @return The value of the cell that resides in both the row and column that were inputted, for the resultant matrix.
      */
-    static double cellMult(double[] row, double[] col) {
+    public static double cellMult(double[] row, double[] col) {
+        //Result to be returned when finished.
         double result = 0;
 
         for (int i = 0; i < SIZE; i++) {
@@ -105,7 +100,7 @@ public class Question6 {
      * @param matrix The matrix to be transposed
      * @return matrix transposed
      */
-    static double[][] matTranspose(double[][] matrix) {
+    public static double[][] matTranspose(double[][] matrix) {
 
         double[][] transposed = new double[matrix.length][matrix[0].length];
 
@@ -116,5 +111,21 @@ public class Question6 {
         }
 
         return transposed;
+    }
+
+    /**
+     * Displays the matrix to the user.
+     *
+     * @param matrix The matrix to be displayed to the user.
+     */
+    public static void displayMatrix(double[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.printf("%7.2f ", matrix[i][j]);
+            }
+            System.out.println();
+            System.out.print("           ");
+        }
     }
 }
