81ebb0169c89d14d93280472c51b5e868ad3de4a
Mark Said Camilleri <markscamilleri@users.noreply.github.com>
2016-05-24 15:32:30 +0200

    Added inline comments

--- CSA1017 Q9/src/Question9.java
+++ CSA1017 Q9/src/Question9.java
@@ -26,13 +26,18 @@ public class Question9 {
 
             try {
                 n = in.nextInt();
+
+                //Only accept positive integers for this.
+                if (n < 1) {
+                    System.out.println("|   Your input was not accepted. Please try again   |");
+                }
             } catch (InputMismatchException ime) {
                 //If input is wrong
                 System.out.println("|   Your input was not accepted. Please try again   |");
-                n = -1;
+                n = 0;
                 in.nextLine();
             }
-        } while (n == -1);
+        } while (n < 1);
 
         double result = fibsum(n);
         System.out.printf("| The sum to the first %-5d terms is: %-12g |\n", n, result);
@@ -50,29 +55,31 @@ public class Question9 {
      * the nth term
      */
     public static double fibsum(int n) {
-        //First result
-        double result = 0;
-
         /*
-         * -1th and 0th term respectively (of course -1th term
-         * doesn't exist. But these are (i-2)th and (i-1)th terms
-         * respectively.
+         * 1st and 2nd term respectively these are (i-2)th and
+         * (i-1)th terms respectively.
          */
         double fib1 = 0;
         double fib2 = 1;
 
-        for (int i = 1; i <= n; i++) {
-            //ith term of fib sequence
-            double fib = fib1 + fib2;
-            result += fib;
+        //Base cases, since the pattern is based on the first 2 terms.
+        if (n == 1) return fib1;
+        else if (n == 2) return fib2;
+        else {
+            //Result of the sum of the first 2 terms, to add to it.
+            double result = 1;
+            for (int i = 3; i <= n; i++) {
+                //ith term of fib sequence
+                double fib = fib1 + fib2;
+                result += fib;
 
-            //Shifts (i-1)th term to (i-2)th term
-            fib1 = fib2;
-            //Shifts ith term to (i-1)th term
-            fib2 = fib;
+                //Shifts (i-1)th term to (i-2)th term
+                fib1 = fib2;
+                //Shifts ith term to (i-1)th term
+                fib2 = fib;
+            }
+            //Sum of fibonacci sequence is returned
+            return result;
         }
-
-        //Sum of fibonacci sequence is returned
-        return result;
     }
 }
